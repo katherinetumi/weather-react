9dec62a5936d4dae66df8030b8b54deee15662ce
itumelengkolwane <162047181+katherinetumi@users.noreply.github.com>
2024-03-27 17:49:31 +0200

    added weather.js weathericon.js and weatherTemperture.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Weather from "./Weather";
 import "./App.css";
 
+
 export default function App() {
   return (
     <div className="App">
--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import WeatherInfo from "./Weatherinfo";
+import WeatherInfo from "./WeatherInfo";
 import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
+
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
